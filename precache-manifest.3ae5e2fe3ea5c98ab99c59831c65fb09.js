15c5923eebb37672b3a6ad6a10e2e6012e3739e2
biotech-solutions <solutionsbiotechec@gmail.com>
2020-04-14 22:43:42 -0500

    Updates

---
+++ precache-manifest.3ae5e2fe3ea5c98ab99c59831c65fb09.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91833a0e76300c1ada28f5c6bc7c0b10",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "c528fed729e241ea7825",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "04745984eb7334a4ad46",
+    "url": "/home/static/css/main.002515ad.chunk.css"
+  },
+  {
+    "revision": "c528fed729e241ea7825",
+    "url": "/home/static/js/2.60d040be.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/home/static/js/2.60d040be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04745984eb7334a4ad46",
+    "url": "/home/static/js/main.f0c75e28.chunk.js"
+  },
+  {
+    "revision": "9a21ece79eae19b95cd8",
+    "url": "/home/static/js/runtime-main.68cf1828.js"
+  },
+  {
+    "revision": "2e0eba3baf28c84ee0cd61daa6079a92",
+    "url": "/home/static/media/Logo blanco.2e0eba3b.png"
+  },
+  {
+    "revision": "8b0f5297c900f390e47f3e28bdd94ec5",
+    "url": "/home/static/media/call.8b0f5297.svg"
+  },
+  {
+    "revision": "48a37360796dcd01f8c05a2b44e765ba",
+    "url": "/home/static/media/contact.48a37360.jpg"
+  },
+  {
+    "revision": "58653ee05c3beac954eb665d129a2bde",
+    "url": "/home/static/media/email.58653ee0.svg"
+  },
+  {
+    "revision": "240629bb90f0f9562927a1b9415caf6f",
+    "url": "/home/static/media/gato1.240629bb.jpg"
+  },
+  {
+    "revision": "5ba49cbe31cf28c9a2c4373ef85bdcea",
+    "url": "/home/static/media/gato2.5ba49cbe.jpg"
+  },
+  {
+    "revision": "4151749336a9ca41b1cd8e1d9f38b208",
+    "url": "/home/static/media/gato3.41517493.jpg"
+  },
+  {
+    "revision": "4c2956aad43803bbffeb390a9163f996",
+    "url": "/home/static/media/gato4.4c2956aa.jpg"
+  },
+  {
+    "revision": "4c9f2c87377325c9e1798577b0f6c1cd",
+    "url": "/home/static/media/linkedin.4c9f2c87.svg"
+  }
+]);
